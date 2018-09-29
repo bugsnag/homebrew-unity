0b43611d3d21ea2844b4dc5a282dcf13da63c3fa
Martin Holman <martin308@gmail.com>
2018-09-28 17:05:25 -0700

    Use the correct SHA

--- Casks/unity-tvos-support-for-editor-5.6.6f2.rb
+++ Casks/unity-tvos-support-for-editor-5.6.6f2.rb
@@ -1,6 +1,6 @@
 cask 'unity-tvos-support-for-editor-5.6.6f2' do
   version '5.6.6f2,6bac21139588'
-  sha256 '29dc9a82561df6f191b61dd6f9ef3943086ca07382b8f1aa1c4708ca537e0b8e'
+  sha256 'b8a6c674c4c07f2c57b772c231c2f7950cd966503d39e225f16d1694809fa8e5'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-AppleTV-Support-for-Editor-#{version.before_comma}.pkg"
   name 'Unity tvOS Build Support'
